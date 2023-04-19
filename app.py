064c49e5c1151ceef52dd1e5fdd1df2b054f9409
walter <belwalterv@gmail.com>
2023-04-18 23:06:27 -0300

    update delete foreing key

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from conexion import (
     db_params,
     get_all_persons,
     get_by_id,
+    delete_by_id,
+    update_person_email,
+    get_person_deptos,
     insert_person,
 )
 
@@ -36,6 +39,12 @@ def get_person_by_id():
     response = get_by_id(id)
     return response
 
+@app.route('/get_person_deptos')
+def get_person_deptos_route():
+    id = request.args['id']
+    response = get_person_deptos(id)
+    return response
+
 @app.route('/insert_person')
 def insert_person_route():
     name = request.args['name']
@@ -46,6 +55,20 @@ def insert_person_route():
         email = None
     response = insert_person(name, last_name, email)
     return f'new person add to database with id {str(response)}'
+
+@app.route('/delete_person_by_id')
+def delete_person_by_id():
+    id = request.args['id']
+    delete_by_id(id)
+    return "delete successfuly"
+
+@app.route('/update_mail_person')
+def update_person_mail():
+    id = request.args['id']
+    email = request.args['email']
+    update_person_email(id, email)
+    return f'email address updated'
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
--- conexion.py
+++ conexion.py
@@ -52,6 +52,31 @@ def get_by_id(id):
     return datos
 
 
+def get_person_deptos(id):
+    connection, con = get_db(db_params)
+    connection.execute(f"select * from departamento d join persona p on (d.fk_persona = p.id) where p.id ={id}")
+    personas = connection.fetchall()
+    datos_persona = {}
+    if len(personas) > 0:
+        datos_persona['id'] = personas[0][3]
+        datos_persona['nombre'] = personas[0][4]
+        datos_persona['apellido'] = personas[0][5]
+        datos_persona['email'] = personas[0][6]
+        datos_persona['deptos'] = []
+        deptos = []
+        for persona in personas:
+            datos_depto = {}
+            datos_depto['id'] = persona[0]
+            datos_depto['nombre'] = persona[1]
+            datos_persona['deptos'].append(datos_depto)
+    
+    return datos_persona
+    
+def delete_by_id(id):
+    connection, con = get_db(db_params)
+    connection.execute(f"delete from persona where id = {id}")
+    con.commit()
+
 def insert_person(name, last_name, email):
     connection, con = get_db(db_params)
     connection.execute(f"insert into persona (name, last_name, email) values ('{name}', '{last_name}', '{email}') returning id")
@@ -59,6 +84,10 @@ def insert_person(name, last_name, email):
     con.commit()
     return persona[0]
 
+def update_person_email(id, email):
+    connection, con = get_db(db_params)
+    connection.execute(f"update persona set email = '{email}' where id = {id}")
+    con.commit()
 # @contextlib.contextmanager
 # def database(params):
 #     print("Connecting to PostgreSQL database...")
